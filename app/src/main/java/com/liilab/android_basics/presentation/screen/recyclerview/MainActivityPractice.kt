37db0bdddc3f1faa62e8c9dbc791c12ee2679950
unknown <promaroy056@gmail.com>
2023-11-06 13:08:23 +0600

    recycle

---
+++ app/src/main/java/com/liilab/android_basics/presentation/screen/recyclerview/MainActivityPractice.kt
@@ -0,0 +1,12 @@
+package com.liilab.android_basics.presentation.screen.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.liilab.android_basics.R
+
+class MainActivityPractice : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_practice)
+    }
+}
